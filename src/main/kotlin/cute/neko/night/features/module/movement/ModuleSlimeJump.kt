f73ebcfe4268427fca8bdb0d9093007fea50d85e
yuchenxue <null>
2025-11-02 22:00:15 +0800

    Update ModuleSlimeJump.kt

--- src/main/kotlin/cute/neko/night/features/module/movement/ModuleSlimeJump.kt
+++ src/main/kotlin/cute/neko/night/features/module/movement/ModuleSlimeJump.kt
@@ -4,49 +4,44 @@ import cute.neko.night.event.events.game.player.PlayerMoveEvent
 import cute.neko.night.event.handler
 import cute.neko.night.features.module.ClientModule
 import cute.neko.night.features.module.ModuleCategory
-import cute.neko.night.features.module.movement.ModuleSlimeJump.Mode.*
-import cute.neko.night.features.setting.type.mode.SubMode
 import cute.neko.night.utils.extensions.getBlock
 import net.minecraft.block.Blocks
+import kotlin.math.min
+import kotlin.math.pow
 
-// From LiquidBounce discord
+// from LiquidBounce discord
+// for grim
+// date 25/11/2
 object ModuleSlimeJump : ClientModule("SlimeJump", ModuleCategory.MOVEMENT) {
 
-    private val mode by mode("Mode", Mode.MODIFY)
-
-    private val velocity by float("Velocity", 1f, 0.5f..5f, 0.05f)
-
-    private val addition by float("Addition", 0.2f, 0f..2f, 0.02f)
-
-    private val max by float("Max", 1f, 0.5f..5f, 0.05f)
-    private val min by float("Min", 0.5f, 0.25f..1.5f, 0.05f)
+    private val pow by float("Pow", 0.75f, 0.1f..1f)
+    private val multiplier by float("Multiplier", 1.4f, 0.1f..2f)
 
     @Suppress("unused")
     private val onPlayerMove = handler<PlayerMoveEvent> { event ->
         if (player.isOnGround) {
             val pos = player.blockPos.down()
+            val movement_y = event.movement.y
 
-            val onSlime = pos.getBlock() == Blocks.SLIME_BLOCK
+            val on_slime = pos.getBlock() == Blocks.SLIME_BLOCK
 
-            if (onSlime && event.movement.y > min && event.movement.y < max) {
+            if (on_slime && movement_y > 0 && movement_y != 0.41999998688697815) {
 
 //                debug.debug("MY:" + event.movement.y)
 
-                when (mode) {
-                    MODIFY -> {
-                        event.movement.y = velocity.toDouble()
-                    }
-                    ADD -> {
-                        event.movement.y += addition.toDouble()
-                    }
-                }
+                event.movement.y = bounce(
+                    movement_y,
+                    pow,
+                    multiplier
+                )
 
             }
         }
     }
 
-    enum class Mode(override val modeName: String) : SubMode {
-        MODIFY("Modify"),
-        ADD("Add")
+    private fun bounce(y: Double, pow: Float, multiplier: Float): Double {
+        val capped = min(y, 1.0)
+        val boosted = capped.pow(pow.toDouble()) * multiplier
+        return boosted.coerceAtMost(1.0)
     }
 }
